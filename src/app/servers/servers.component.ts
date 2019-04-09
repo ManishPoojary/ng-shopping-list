3b893ede739475d217838edf4cecd00088e9f552
ManishPoojary <manissh2327@gmail.com>
2019-04-09 11:40:16 +0530

    if and For

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No Server was Created!";
   serverName='TestServer';
+  serverCreated=false;
+  servers = ['Testserver', 'Testserver 2']
   constructor() {
     setTimeout(()=>{
       this.allowNewServer=true;
@@ -19,6 +21,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
+    this.serverCreated=true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server Was Created!'
   }
 
